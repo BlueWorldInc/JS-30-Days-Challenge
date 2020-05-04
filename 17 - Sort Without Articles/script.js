61d56dc009965d525253cf3009417a9746d3ebc5
BlueWorldInc <keamybakunin@gmail.com>
2020-05-04 13:26:32 +0200

    [refactor] changed a bit

--- 17 - Sort Without Articles/script.js
+++ 17 - Sort Without Articles/script.js
@@ -1,38 +1,33 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything',
     'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-let orderedBands = [];
-
-for (let i = 0; i < bands.length; i++) {
-    orderedBands.push(getNameOfBand(bands[i]));
-}
+let orderedBands = bands.slice(0);
 
 orderedBands.sort(function(a, b) { 
-    var x = a.newBand.toLowerCase();
-    var y = b.newBand.toLowerCase();
+    var x = getNameOfBand(a.toLowerCase());
+    var y = getNameOfBand(b.toLowerCase());
     return x < y ? -1 : x > y ? 1 : 0;
 })
 
 
 for (const b of orderedBands) {
-    document.getElementById("bands").innerHTML += "<li>" + b.band + "</li>";
+    document.getElementById("bands").innerHTML += "<li>" + b + "</li>";
 }
 
 function getNameOfBand(band) {
     let bandArray = band.split(' ');
     switch (bandArray[0]) {
-        case "A":
+        case "a":
             bandArray.shift();
             break;
-        case "An":
+        case "an":
             bandArray.shift();
             break;
-        case "The":
+        case "the":
             bandArray.shift();
             break;
         default:
             break;
     }
-    newBand  = bandArray.join(' ');
-    return {band: band, newBand: newBand};
+    return bandArray.join(' ');
 }
